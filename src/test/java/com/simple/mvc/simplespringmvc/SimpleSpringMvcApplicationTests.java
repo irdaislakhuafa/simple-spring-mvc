0e551bbaefc252679053a8af23efc4048ff6d7d1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-10-18 11:18:19 +0700

    First commit

---
+++ src/test/java/com/simple/mvc/simplespringmvc/SimpleSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.mvc.simplespringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

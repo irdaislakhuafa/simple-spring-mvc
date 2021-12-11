00d6c1f4f0909ae9970ae881495d74c99a35ea0e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-12-11 23:26:35 +0700

    add some anotations in Product.java

--- src/main/java/com/simple/mvc/models/entity/Product.java
+++ src/main/java/com/simple/mvc/models/entity/Product.java
@@ -1,6 +1,12 @@
 package com.simple.mvc.models.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +16,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+
+    @Column(length = 10, nullable = false, unique = true)
     private String code;
+
+    @Column(length = 100, nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private Double price;
+
+    @Column(length = 1000, nullable = false)
     private String description;
 }
